0d24aba3a732d4f8049e9b520cff6be48982e5e5
Maicon Diniz Filippsen <filippsen@blockie.org>
2018-07-09 12:04:05 +0200

    Bump version to 1.0-beta22

--- src/components/app.js
+++ src/components/app.js
@@ -32,7 +32,7 @@ export default class App extends Component {
         super(props);
         this.idCounter=0;
 
-        this._version="1.0-beta21";
+        this._version="1.0-beta22";
         this.session={
             start_time: Date.now(),
         };
