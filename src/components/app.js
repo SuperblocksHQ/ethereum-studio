509f83913a7b15e41517bb32b41201b53a8afdc6
Javier Tarazaga <javier.tarazaga@valtech.se>
2018-07-26 10:18:54 +0200

    Minor fix for bottom bar

--- src/components/app.js
+++ src/components/app.js
@@ -237,7 +237,7 @@ export default class App extends Component {
                     </div>
                     <div class="bottom-status-bar">
     					<span class="left">
-                            <span class="left note">Note</span>
+                            <span class="note">Note</span>
                             <span class="note-text">All files are stored in the browser only, download to backup</span>
                         </span>
                         <span class="right">{this._version}</span>
