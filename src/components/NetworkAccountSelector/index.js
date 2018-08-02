fca52f2eab34405933a2f619033077c22b0266f3
Javier Tarazaga <javier.tarazaga@valtech.se>
2018-08-02 14:10:16 +0200

    Minor fix in the networkaccount selector

--- src/components/NetworkAccountSelector/index.js
+++ src/components/NetworkAccountSelector/index.js
@@ -8,7 +8,7 @@ class NetworkSelector extends Component {
             <div class={ style.selector }>
                 Browser
                 <div class={ style.dropdownIcon }>
-                    <IconDropdown height="10" width="12"/>
+                    <IconDropdown height="8" width="10"/>
                 </div>
             </div>
         )
@@ -21,7 +21,7 @@ class AccountSelector extends Component {
             <div class={ style.selector }>
                 Default
                 <div class={ style.dropdownIcon }>
-                    <IconDropdown height="10" width="12"/>
+                    <IconDropdown height="8" width="10"/>
                 </div>
             </div>
         )
