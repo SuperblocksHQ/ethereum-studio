dbd9feb858967bcc14850f633e2d3ade7ea34be8
Thomas Backlund <thomas@superblocks.com>
2018-09-05 18:31:40 +0200

    Bump version to 1.0-beta25

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -30,7 +30,7 @@ export default class App extends Component {
         super(props);
         this.idCounter=0;
 
-        this._version="1.0-beta24";
+        this._version="1.0-beta25";
         this.session={
             start_time: Date.now(),
         };
