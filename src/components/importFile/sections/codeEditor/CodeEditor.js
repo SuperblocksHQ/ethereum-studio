b8cf5c98679096a01f4929a7693595932abc4911
donfrigo <donfrigo@gmail.com>
2018-11-16 11:51:26 +0100

    added Monaco editor to import

---
+++ src/components/importFile/sections/codeEditor/CodeEditor.js
@@ -0,0 +1,57 @@
+// Copyright 2018 Superblocks AB
+//
+// This file is part of Superblocks Lab.
+//
+// Superblocks Lab is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+//
+// Superblocks Lab is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
+
+import React, { Component } from 'react';
+import style from './style.less';
+import MonacoEditor from 'react-monaco-editor';
+
+export default class CodeEditor extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    editorDidMount(editor, monaco) {
+        console.log('editorDidMount', editor);
+        editor.focus();
+    }
+
+    onChange(newValue, e) {
+        console.log('onChange', newValue, e);
+    }
+
+    render() {
+        const options = {
+            selectOnLineNumbers: false,
+            readOnly: true
+        };
+
+        return (
+            <MonacoEditor
+                width="600"
+                height="400"
+                language="javascript"
+                theme="vs-dark"
+                value={this.props.source}
+                options={options}
+                onChange={this.onChange}
+                editorDidMount={this.editorDidMount}
+            />
+        );
+    }
+}
+
+
+
