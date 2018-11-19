1677ec819f6244529c1f6766713665065c815857
donfrigo <donfrigo@gmail.com>
2018-11-19 10:35:35 +0100

    Added code to generate the file structure of the openzeppelin library and an empty directory to store generated files.

--- scripts/build.js
+++ scripts/build.js
@@ -187,3 +187,105 @@ function copyPublicFolder() {
     filter: file => file !== paths.appHtml,
   });
 }
+
+// Generate OpenZeppelin file structure
+
+// Directory, where smart contracts are stored
+const contractsDirectory = path.join(__dirname, '../', 'node_modules', 'openzeppelin-solidity', 'contracts');
+
+// Directory, where json contract files are stored
+const jsonDirectory = path.join(__dirname, '../', 'node_modules', 'openzeppelin-solidity', 'build', 'contracts');
+
+// Path of the resulting file
+const jsonFilePath = path.join(__dirname, '../', 'src', 'assets', 'static', 'json', 'openzeppelin.json');
+
+// Read all Solidity files from directory
+const getAllSolidityFiles = dir =>
+    fs.readdirSync(dir).reduce((files, file) => {
+        const name = path.join(dir, file);
+        const isDirectory = fs.statSync(name).isDirectory();
+        const isSolidityFile = path.extname(name) === '.sol';
+
+        const trimmedPath = name.split("openzeppelin-solidity/")[1]
+        return isDirectory
+            ? [...files, ...getAllSolidityFiles(name)]
+            : isSolidityFile
+                ? [...files, trimmedPath ]
+                : [...files]
+}, []);
+
+// Build file tree from path
+function buildTree(paths) {
+
+    var currentPath, lastPath, node, parent, map = {
+            "": {
+                children: []
+            }
+        },
+        stack = [""]
+
+    let counter = 0;
+    for (let path of paths) {
+        let nodes = path.split("/");
+        for (let i = 0; i < nodes.length; i++) {
+            currentPath = "/" + nodes.slice(1, i + 1).join("/")
+            lastPath = stack[stack.length - 1]
+            parent = map[lastPath]
+            if (!map[currentPath]) {
+                let nameSlice = currentPath.split('/');
+                let name = nameSlice[nameSlice.length-1];
+                if (currentPath.endsWith('.sol')){
+                    // file
+                    node = {
+                        name: name,
+                        id: counter,
+                        source: getFileJSON(jsonDirectory, name).source
+                    }
+                } else {
+                    // folder
+                    node = {
+                        name: i === 0 ? "contracts" : name,
+                        toggled: i === 0 && true,
+                        id: counter,
+                        children: []
+                    }
+                }
+                parent.children.push(node);
+                map[currentPath] = node;
+                counter ++
+            }
+            stack.push(currentPath)
+        }
+        stack = stack.slice(0, 1)
+    }
+    return map[""].children[0];
+}
+
+// Read .json file
+function getFileJSON(jsonFolderPath, fileName) {
+    fileName = path.parse(fileName).name.concat('.json');
+    let filePath = path.join(jsonFolderPath, fileName);
+
+    try {
+        const raw = fs.readFileSync(filePath);
+        return JSON.parse(raw);
+    }
+    catch (err) {
+        try {
+            // fix for one of the contracts
+            fileName = "ERC20".concat(fileName)
+            filePath = path.join(jsonFolderPath, fileName);
+            const raw = fs.readFileSync(filePath);
+            return JSON.parse(raw);
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
+let tree = buildTree(getAllSolidityFiles(contractsDirectory))
+
+fs.writeFile(jsonFilePath, JSON.stringify(tree), 'utf8', function (err) {
+    if (err) throw err;
+    console.log('OpenZeppelin .json successfully file created.');
+});
