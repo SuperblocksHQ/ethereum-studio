bc85ad3e2f36feaff3378c0bcdbe54d86dd17ee0
Javier Tarazaga <javier@superblocks.com>
2018-12-28 16:32:30 +0100

    Minor fix

--- src/epics/ipfs/restoreIPFSState.epic.js
+++ src/epics/ipfs/restoreIPFSState.epic.js
@@ -1,13 +1,8 @@
 import { from, of } from 'rxjs';
-import { switchMap, withLatestFrom, tap, map, catchError } from 'rxjs/operators';
+import { switchMap, withLatestFrom, map, catchError } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { getSelectedProjectId } from '../../selectors/projects';
 import { ipfsActions, projectActions } from '../../actions';
-
-const extractInfo = (ipfsFileObject) => {
-    console.log(ipfsFileObject);
-    return ipfsFileObject.shareURL;
-}
 
 const restoreIPFSState = (action$, state$, { backend }) => action$.pipe(
     ofType(projectActions.SELECT_PROJECT),
@@ -17,9 +12,11 @@ const restoreIPFSState = (action$, state$, { backend }) => action$.pipe(
         return from(backend.loadFilePromise(projectId, '/.super/ipfs.json'))
         .pipe(
             map(JSON.parse),
-            // map(extractInfo),
             map(ipfsActions.restoreIPFSStateSuccess),
-            catchError(() => of(ipfsActions.restoreIPFSStateFail()))
+            catchError((error) => {
+                console.log(error);
+                of(ipfsActions.restoreIPFSStateFail())
+            })
         )
     })
 );
