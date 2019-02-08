11fd7bccf823cf1050cdb4429be7778934e92e12
Javier Tarazaga <javier@superblocks.com>
2019-02-08 17:57:58 +0100

    Changed import

--- src/epics/login/silentLogin.epic.ts
+++ src/epics/login/silentLogin.epic.ts
@@ -19,7 +19,7 @@ import { ofType } from 'redux-observable';
 import { appActions, authActions } from '../../actions';
 import { withLatestFrom, tap, switchMap, catchError, map } from 'rxjs/operators';
 import { AnyAction } from 'redux';
-import { userService } from '../../services/user.service';
+import { userService } from '../../services';
 
 // TODO - Naive implementation. Make sure if we get a 401 we try to get a new token with the refresh token.
 export const silentLogin = (action$: AnyAction, state$: any) => action$.pipe(
