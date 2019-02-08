f12fc25bff7cda953ffe393bfcfb0f4c064e6d2e
Javier Tarazaga <javier@superblocks.com>
2019-02-08 17:55:58 +0100

    Implemented a naive silentLogin

---
+++ src/epics/login/silentLogin.epic.ts
@@ -0,0 +1,37 @@
+// Copyright 2019 Superblocks AB
+//
+// This file is part of Superblocks Lab.
+//
+// Superblocks Lab is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+//
+// Superblocks Lab is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
+
+import { empty, from } from 'rxjs';
+import { ofType } from 'redux-observable';
+import { appActions, authActions } from '../../actions';
+import { withLatestFrom, tap, switchMap, catchError, map } from 'rxjs/operators';
+import { AnyAction } from 'redux';
+import { userService } from '../../services/user.service';
+
+// TODO - Naive implementation. Make sure if we get a 401 we try to get a new token with the refresh token.
+export const silentLogin = (action$: AnyAction, state$: any) => action$.pipe(
+    ofType(appActions.APP_START),
+    withLatestFrom(state$),
+    switchMap(() => from(userService.getUser())
+        .pipe(
+            map(authActions.loginSuccess)
+        )
+    ),
+    catchError((err: any) => {
+        console.log('Error: ', err);
+        return empty();
+    })
+);
