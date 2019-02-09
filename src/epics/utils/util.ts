3f8afef53d47240a51713c02272ba22a373e6575
donfrigo <donfrigo@gmail.com>
2019-02-09 22:27:33 +0900

    Silentlogin bugfixes

---
+++ src/epics/utils/util.ts
@@ -0,0 +1,36 @@
+import {Observable} from 'rxjs';
+import { getAuthToken } from '../../services/utils/superFetch';
+
+export function createAuthorizedHttpObservable(url: any) {
+    return Observable.create((observer: any) => {
+
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getAuthToken()}`
+            },
+            signal
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    observer.error({errorCode: response.status});
+                }
+            })
+            .then(body => {
+                observer.next(body);
+                observer.complete();
+            })
+            .catch(err => {
+                observer.error(err);
+            });
+
+        return () => controller.abort();
+
+    });
+}
+
