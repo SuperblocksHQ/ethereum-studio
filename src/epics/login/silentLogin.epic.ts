f9e8d7066e8f29af0d051efa34e62b2c669ddc5d
donfrigo <donfrigo@gmail.com>
2019-02-12 18:29:55 +0900

    Silentlogin using userService revert

--- src/epics/login/silentLogin.epic.ts
+++ src/epics/login/silentLogin.epic.ts
@@ -14,23 +14,18 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import {empty, pipe} from 'rxjs';
+import {empty, from, pipe} from 'rxjs';
 import { ofType } from 'redux-observable';
 import { appActions, authActions } from '../../actions';
 import {withLatestFrom, tap, catchError, map, flatMap} from 'rxjs/operators';
 import { AnyAction } from 'redux';
-import {createAuthorizedHttpObservable} from '../utils/util';
-
-const login = () => {
-    return createAuthorizedHttpObservable(
-        process.env.REACT_APP_API_BASE_URL + '/user');
-};
+import { userService } from '../../services';
 
 // TODO - Naive implementation. Make sure if we get a 401 we try to get a new token with the refresh token.
 export const silentLogin = (action$: AnyAction, state$: any) => action$.pipe(
     ofType(appActions.APP_START),
     withLatestFrom(state$),
-    flatMap(() => login()),
+    flatMap(() => from(userService.getUser())),
     pipe(
         map(authActions.loginSuccess),
         tap(() => console.log('Silent login success'))
