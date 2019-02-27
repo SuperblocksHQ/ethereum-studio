2695a5e89ed3c5f3716ef1f0d96d3fa30ff8ab62
Javier Tarazaga <javier@superblocks.com>
2019-02-27 13:03:58 +0100

    Removed Learn and Resources from explorer panel

--- src/components/projecteditor/panels/explorer/explorer.tsx
+++ src/components/projecteditor/panels/explorer/explorer.tsx
@@ -1,22 +1,21 @@
 // Copyright 2019 Superblocks AB
-// 
+//
 // This file is part of Superblocks Lab.
-// 
+//
 // Superblocks Lab is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation version 3 of the License.
-// 
+//
 // Superblocks Lab is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from 'react';
 import style from './style.less';
-import LearnAndResources from '../../../learnAndResources';
 import { FolderItem, FileItem, ContractItem } from './items';
 import { IProjectItem, ProjectItemTypes } from '../../../../models';
 
@@ -112,7 +111,6 @@ export class Explorer extends React.Component<IProps> {
         return (
             <div className={ style.treeContainer }>
                 { treeHtml }
-                <LearnAndResources className='mt-3' />
             </div>
         );
     }
