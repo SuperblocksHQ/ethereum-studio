585cba2679a96987fb90ec2c6c3a0e2608a13374
donfrigo <donfrigo@gmail.com>
2019-03-13 17:47:40 +0900

    added functions generateItem and insert

--- src/reducers/utils/fileUtils.ts
+++ src/reducers/utils/fileUtils.ts
@@ -15,6 +15,7 @@
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
 import { IProjectItem, ProjectItemTypes } from '../../models';
+import { generateUniqueId } from '../../services/utils';
 
 export function isSolitidyFile(item: IProjectItem): boolean {
     return item.type === ProjectItemTypes.File && item.name.toLowerCase().endsWith('.sol');
@@ -26,4 +27,34 @@ const WINDOWS_RESERVED_CHARS_REGEX = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])$/i;
 export function isValidProjectItemName(name: string): boolean {
     name = name.trim();
     return name.length > 0 && name.length < 255 && !UNIX_RESERVED_CHARS_REGEX.test(name) && !WINDOWS_RESERVED_CHARS_REGEX.test(name);
+}
+
+export function generateItem(name: string, type: ProjectItemTypes, code?: string): IProjectItem {
+    return {
+        id: generateUniqueId(),
+        name,
+        mutable: true,
+        type,
+        opened: false,
+        children: [],
+        code
+    };
+}
+
+// Insert path into directory tree structure:
+export function insert(children: any[], [head, ...tail]: any, code: string): IProjectItem[] {
+    let child = children.find((item: IProjectItem)  => item.name === head);
+    if (!child) {
+        if (head.endsWith('.sol')) {
+            // file
+            children.push(child = generateItem(head, ProjectItemTypes.File, code));
+        } else {
+            // folder
+            children.push(child = generateItem(head, ProjectItemTypes.Folder));
+        }
+    }
+    if (tail.length > 0) {
+        insert(child.children, tail, code);
+    }
+    return children;
 }
