09fb2c4d7b529fb575fd0f95ea3edd8a87901580
donfrigo <donfrigo@gmail.com>
2019-03-18 20:32:58 +0900

    forking when renaming an item

--- src/epics/explorer/renameItem.epic.ts
+++ src/epics/explorer/renameItem.epic.ts
@@ -14,32 +14,58 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import { switchMap, catchError, tap, map } from 'rxjs/operators';
+import { switchMap, catchError, map } from 'rxjs/operators';
 import { ofType, Epic } from 'redux-observable';
-import { explorerActions, panesActions } from '../../actions';
+import { explorerActions, projectsActions } from '../../actions';
 import { projectSelectors } from '../../selectors';
 import { projectService } from '../../services';
-import { empty } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
+import { fetchJSON } from '../../services/utils/fetchJson';
 
 export const renameItemEpic: Epic = (action$, state$) => action$.pipe(
     ofType(explorerActions.RENAME_ITEM),
     switchMap((action) => {
         const project = projectSelectors.getProject(state$.value);
         const explorerState = state$.value.explorer;
+        const isOwnProject = state$.value.projects.isOwnProject;
 
         if (explorerState.itemNameValidation.isValid) {
-            return projectService.putProjectById(project.id, {
-                name: project.name,
-                description: project.description,
-                files: explorerState.tree
-            })
-            .pipe(
-                map(() => explorerActions.renameItemSuccess(action.data.id, action.data.name)),
-                catchError(() => [ explorerActions.renameItemFail(explorerState.itemNameValidation.itemId, explorerState.itemNameValidation.oldName) ])
-            );
+            if (isOwnProject) {
+                return projectService.putProjectById(project.id, {
+                    name: project.name,
+                    description: project.description,
+                    files: explorerState.tree
+                })
+                    .pipe(
+                        map(() => explorerActions.renameItemSuccess(action.data.id, action.data.name)),
+                        catchError(() => [ explorerActions.renameItemFail(explorerState.itemNameValidation.itemId, explorerState.itemNameValidation.oldName) ])
+                    );
+            } else {
+                // fork with new filename
+                return projectService.createProject({
+                    name: project.name,
+                    description: project.description,
+                    files: explorerState.tree
+                }).pipe(
+                    switchMap((newProject) =>  {
+                        if (newProject.anonymousToken) {
+                            fetchJSON.setAnonymousToken(newProject.anonymousToken);
+                        }
+
+                        // redirect
+                        window.location.href = `${window.location.origin}/${newProject.id}`;
+
+                        return [explorerActions.renameItemSuccess(action.data.id, action.data.name), projectsActions.forkProjectSuccess()];
+                    }),
+                    catchError((error) => {
+                        console.log('There was an issue forking the project: ' + error);
+                        return of(projectsActions.forkProjectFail(error.message));
+                    })
+                );
+            }
         } else {
             alert('Invalid file or folder name.');
-            return empty();
+            return EMPTY;
         }
     })
 );
