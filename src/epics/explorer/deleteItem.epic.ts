e1b67ca0e015eb92f5796bc682a2c729f7dc25c8
donfrigo <donfrigo@gmail.com>
2019-03-18 20:32:39 +0900

    forking when deleting an item

--- src/epics/explorer/deleteItem.epic.ts
+++ src/epics/explorer/deleteItem.epic.ts
@@ -14,30 +14,58 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import { switchMap } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { ofType, Epic } from 'redux-observable';
-import { explorerActions, panesActions } from '../../actions';
+import { explorerActions, panesActions, projectsActions } from '../../actions';
 import { projectSelectors } from '../../selectors';
 import { projectService } from '../../services';
 import { updateItemInTree } from '../../reducers/explorerLib';
+import { fetchJSON } from '../../services/utils/fetchJson';
+import { of } from 'rxjs';
 
 export const deleteItemEpic: Epic = (action$, state$) => action$.pipe(
     ofType(explorerActions.DELETE_ITEM),
     switchMap((action) => {
         const project = projectSelectors.getProject(state$.value);
         const explorerState = state$.value.explorer;
+        const isOwnProject = state$.value.projects.isOwnProject;
 
-        return projectService.putProjectById(project.id, {
-            name: project.name,
-            description: project.description,
-            // TODO: remove usage of updateItemInTree. This is done only temporary.
-            files: updateItemInTree(explorerState.tree, explorerState.lastDeletedId, () => null)[0]
-        })
-        .pipe(
-            switchMap(() => {
-                return [panesActions.closePane(action.data.id), explorerActions.deleteItemSuccess(action.data.id)];
+        if (isOwnProject) {
+            return projectService.putProjectById(project.id, {
+                name: project.name,
+                description: project.description,
+                // TODO: remove usage of updateItemInTree. This is done only temporary.
+                files: updateItemInTree(explorerState.tree, explorerState.lastDeletedId, () => null)[0]
             })
-            // TODO: error handling
-        );
+                .pipe(
+                    switchMap(() => {
+                        return [panesActions.closePane(action.data.id), explorerActions.deleteItemSuccess(action.data.id)];
+                    }),
+                    catchError(() => [explorerActions.deleteItemFail(action.data.id)])
+                );
+        } else {
+            // fork with new tree structure
+            return projectService.createProject({
+                name: project.name,
+                description: project.description,
+                // TODO: remove usage of updateItemInTree. This is done only temporary.
+                files: updateItemInTree(explorerState.tree, explorerState.lastDeletedId, () => null)[0]
+            }).pipe(
+                switchMap((newProject) =>  {
+                    if (newProject.anonymousToken) {
+                        fetchJSON.setAnonymousToken(newProject.anonymousToken);
+                    }
+
+                    // redirect
+                    window.location.href = `${window.location.origin}/${newProject.id}`;
+
+                    return [panesActions.closePane(action.data.id), explorerActions.deleteItemSuccess(action.data.id), projectsActions.forkProjectSuccess()];
+                }),
+                catchError((error) => {
+                    console.log('There was an issue forking the project: ' + error);
+                    return of(projectsActions.forkProjectFail(error.message));
+                })
+            );
+        }
     })
 );
