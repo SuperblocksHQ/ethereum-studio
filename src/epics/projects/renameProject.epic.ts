eaf6170ab368a104bcddcc44077a677916ac568c
donfrigo <donfrigo@gmail.com>
2019-03-18 18:46:17 +0900

    fork upon renaming

--- src/epics/projects/renameProject.epic.ts
+++ src/epics/projects/renameProject.epic.ts
@@ -14,27 +14,55 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import { switchMap, withLatestFrom, map, tap } from 'rxjs/operators';
+import { switchMap, withLatestFrom, map, catchError } from 'rxjs/operators';
 import { ofType, Epic } from 'redux-observable';
 import { projectsActions } from '../../actions';
 import { projectService } from '../../services/project.service';
 import { projectSelectors } from '../../selectors';
+import { of } from 'rxjs';
+import { fetchJSON } from '../../services/utils/fetchJson';
 
-// TODO - Make sure to handle errors correctly
 export const renameProjectEpic: Epic = (action$: any, state$: any) => action$.pipe(
     ofType(projectsActions.RENAME_PROJECT),
     withLatestFrom(state$),
     switchMap(([action, state]) => {
         const project = projectSelectors.getProject(state);
+        const isOwnProject = state.projects.isOwnProject;
 
-        return projectService.putProjectById(project.id, {
-            name: action.data.newName,
-            description: project.description,
-            files: state.explorer.tree
-        })
-        .pipe(
-            switchMap(() => projectService.getProjectById(project.id)),
-            map(projectsActions.updateProjectSuccess),
-        );
-    })
+        if (isOwnProject) {
+            // update
+            return projectService.putProjectById(project.id, {
+                name: action.data.newName,
+                description: project.description,
+                files: state.explorer.tree
+            })
+                .pipe(
+                    switchMap(() => projectService.getProjectById(project.id)),
+                    map(projectsActions.updateProjectSuccess),
+                );
+        } else {
+            // fork with new name
+            return projectService.createProject({
+                name: action.data.newName,
+                description: project.description,
+                files: state.explorer.tree
+            }).pipe(
+                switchMap((newProject) =>  {
+                    if (newProject.anonymousToken) {
+                        fetchJSON.setAnonymousToken(newProject.anonymousToken);
+                    }
+
+                    // redirect
+                    window.location.href = `${window.location.origin}/${newProject.id}`;
+
+                    return [projectsActions.updateProjectSuccess(newProject), projectsActions.forkProjectSuccess()];
+                }),
+                catchError((error) => {
+                    console.log('There was an issue forking the project: ' + error);
+                    return of(projectsActions.forkProjectFail(error.message));
+                })
+            );
+        }
+    }),
+    catchError(err => [projectsActions.renameProjectFail(err)])
 );
