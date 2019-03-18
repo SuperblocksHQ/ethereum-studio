3cbec6ffd72b3b94a943caf055181806fc197160
donfrigo <donfrigo@gmail.com>
2019-03-18 20:32:47 +0900

    forking when moving an item

--- src/epics/explorer/moveItem.epic.ts
+++ src/epics/explorer/moveItem.epic.ts
@@ -16,24 +16,51 @@
 
 import { switchMap, catchError, map } from 'rxjs/operators';
 import { ofType, Epic } from 'redux-observable';
-import { explorerActions, panesActions } from '../../actions';
+import { explorerActions, projectsActions } from '../../actions';
 import { projectSelectors } from '../../selectors';
 import { projectService } from '../../services';
+import { fetchJSON } from '../../services/utils/fetchJson';
+import { of } from 'rxjs';
 
 export const moveItemEpic: Epic = (action$, state$) => action$.pipe(
     ofType(explorerActions.MOVE_ITEM),
     switchMap((data) => {
         const project = projectSelectors.getProject(state$.value);
         const explorerState = state$.value.explorer;
+        const isOwnProject = state$.value.projects.isOwnProject;
 
-        return projectService.putProjectById(project.id, {
-            name: project.name,
-            description: project.description,
-            files: explorerState.tree
-        })
-        .pipe(
-            map(() => explorerActions.moveItemSuccess(data.sourceId)),
-            catchError(() => [ explorerActions.moveItemFail(data.sourceId) ])
-        );
+        if (isOwnProject) {
+            return projectService.putProjectById(project.id, {
+                name: project.name,
+                description: project.description,
+                files: explorerState.tree
+            })
+                .pipe(
+                    map(() => explorerActions.moveItemSuccess(data.sourceId)),
+                    catchError(() => [ explorerActions.moveItemFail(data.sourceId) ])
+                );
+        } else {
+            // fork with new tree structure
+            return projectService.createProject({
+                name: project.name,
+                description: project.description,
+                files: explorerState.tree
+            }).pipe(
+                switchMap((newProject) =>  {
+                    if (newProject.anonymousToken) {
+                        fetchJSON.setAnonymousToken(newProject.anonymousToken);
+                    }
+
+                    // redirect
+                    window.location.href = `${window.location.origin}/${newProject.id}`;
+
+                    return [explorerActions.moveItemSuccess(data.sourceId), projectsActions.forkProjectSuccess()];
+                }),
+                catchError((error) => {
+                    console.log('There was an issue forking the project: ' + error);
+                    return of(projectsActions.forkProjectFail(error.message));
+                })
+            );
+        }
     })
 );
