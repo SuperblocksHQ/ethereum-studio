1553091f58d7a5328201b8ad4a94766e1babe80d
Maicon Diniz Filippsen <filippsen@users.noreply.github.com>
2019-03-25 17:56:35 +0100

    [WIP] Bump version to 1.7.0 (#402)
    Bump version to 1.7.0

--- src/reducers/app.reducer.ts
+++ src/reducers/app.reducer.ts
@@ -18,7 +18,7 @@ import { AnyAction } from 'redux';
 import { appActions } from '../actions';
 
 export const initialState = {
-    version: '1.6.1',
+    version: '1.7.0',
     isEmbeddedMode: false
 };
 
