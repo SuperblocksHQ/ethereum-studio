a29ec5aa752c5f13e10132bbf627c323c27f667b
Kryštof Viktora <krystof.viktora@seznam.cz>
2019-03-25 14:39:15 +0100

    Show loader when forking project (#386)
    * Implement loader when forking project
    
    * Fix console errors

--- src/selectors/user.selectors.ts
+++ src/selectors/user.selectors.ts
@@ -17,5 +17,6 @@
 export const userSelectors = {
     getProjectList: (state: any) => state.user.projectList,
     getUserProfile: (state: any) => state.user.profile,
-    isProjectListLoading: (state: any) => state.user.isProjectListLoading
+    isProjectListLoading: (state: any) => state.user.isProjectListLoading,
+    isProjectForking: (state: any) => state.user.isProjectForking,
 };
