57dd1b54cf84cc2fb76eacd7fe03959d1b990f95
donfrigo <donfrigo@gmail.com>
2019-03-27 21:51:15 +0900

    added ITokenExpiration model

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,4 +1,4 @@
-// Copyright 2018 Superblocks AB
+// Copyright 2019 Superblocks AB
 //
 // This file is part of Superblocks Lab.
 //
@@ -14,10 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-export * from './contractAgrumentData';
-// TODO: fix!
-export * from './project';
-export * from './project.model';
-export * from './user.model';
-export * from './account-environment.model';
-export * from './auth.model';
+export interface ITokenExpiration {
+    nextRefresh: number;
+    refreshInterval: number;
+}
