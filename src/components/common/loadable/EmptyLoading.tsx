cda40069cc5ecc2f171931300932edd9e0919c81
donfrigo <donfrigo@gmail.com>
2019-04-14 00:55:35 +0900

    added loading screens

--- src/components/common/loadable/EmptyLoading.tsx
+++ src/components/common/loadable/EmptyLoading.tsx
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-export * from './Loading';
-export * from './EmptyLoading';
-export * from './GenericLoading';
+import React, {Fragment} from 'react';
+
+export class EmptyLoading extends React.Component<
+    any
+    > {
+    render() {
+        return (
+            <Fragment />
+        );
+    }
+}
