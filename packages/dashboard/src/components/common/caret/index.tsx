82f052c17453f80f9deb3ae2cd598afc5a162e6c
Javier Tarazaga <javier@superblocks.com>
2019-04-20 09:42:42 +0200

    Converted caret to Typescript

--- packages/dashboard/src/components/common/caret/index.tsx
+++ packages/dashboard/src/components/common/caret/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import style from './style.less';
 import { IconAngleRight, IconAngleDown } from '../../icons';
 import classNames from 'classnames';
 
-export const Caret = ({ onClick, expanded = false } = props) => (
+interface IProps {
+    expanded: boolean;
+    onClick?: () => void;
+}
+export const Caret = ({ onClick, expanded = false }: IProps) => (
     <div className={classNames(style.caret, expanded ? style.caretExpanded : null)} onClick={onClick}>
         {expanded ? (
-            <IconAngleDown height="5" width="8" />
+            <IconAngleDown height='5' width='8' />
         ) : (
-            <IconAngleRight height="8" width="5" />
+            <IconAngleRight height='8' width='5' />
         )}
     </div>
 );
-
-Caret.propTypes = {
-    expanded: PropTypes.bool.isRequired,
-    onClick: PropTypes.func,
-};
