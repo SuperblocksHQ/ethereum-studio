423433e1dbd56201b806a8debc671134ff8f7488
Spejbl <dedecekk@gmail.com>
2019-04-30 11:14:57 +0200

    Remove duplicate

---
+++ packages/dashboard/src/components/common/modal/Modal.tsx
@@ -0,0 +1,31 @@
+// Copyright 2019 Superblocks AB
+//
+// This file is part of Superblocks Lab.
+//
+// Superblocks Lab is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 3 of the License.
+//
+// Superblocks Lab is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
+
+import React from  'react';
+import style from './style.less';
+
+interface IProps {
+    children: JSX.Element | JSX.Element[];
+    onClose: () => void;
+}
+
+export function Modal(props: IProps) {
+    return (
+        <div className={style.modalContainer} onClick={props.onClose}>
+            <div className={style.modal}>{props.children}</div>
+        </div>
+    );
+}
