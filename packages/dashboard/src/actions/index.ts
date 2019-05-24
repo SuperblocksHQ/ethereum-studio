ea968c094ad49ae52afc7397243f9b9b45e9efa0
Javier Tarazaga <javier@superblocks.com>
2019-05-24 12:10:37 +0200

    Removed dashboard references and leave superblocks-lab stuff only

--- packages/dashboard/src/actions/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './app.actions';
-export * from './projects.actions';
-export * from './toast.actions';
-export * from './user.actions';
-export * from './auth.actions';
-export * from './organization.actions';
-export * from './pipelines.actions';
--- packages/dashboard/src/components/common/tooltip/index.js
+++
@@ -1,4 +0,0 @@
-import 'react-tippy/dist/tippy.css';
-import { Tooltip, withTooltip } from 'react-tippy';
-
-export { Tooltip, withTooltip };
