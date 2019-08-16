3a13b1e94ed9a40df0e8eebed526994bc9199833
Maicon Diniz Filippsen <filippsen@users.noreply.github.com>
2019-08-16 11:09:35 +0200

    Update Solidity compiler (#430)
    * Update Solidity compiter to version 0.5.10
    
    * Update OpenZeppelin package to version 2.3.0
    
    * Rebuild solc iframe to make sure it synchronizes with latest changes
    
    * Update pragma statement version for all template contract files
    
    * Update template contracts to use explicit memory modifier where
    applicable
    
    * Change cryptopizza generateRandomDna function from external to public
    
    * Update ERC721 contract interface in Crypto Pizza template
    
    * Update all address comparisons in example contracts to involve
    explicit type casting
    
    * Change constant keyword usage in template contracts in favor of the
    view keyword
    
    * Explicitly add the payable modifier to receiver/beneficiary address in
    Raise To Summon template
    
    * Explicitly add address type casting to this keyword in all contract
    templates
    
    * Change the usage of var keyword in favor of explicit data type in all
    contract templates
    
    * Update Ownable contract in Voting template to use latest OpenZeppelin
    implementation
    
    * Change test compiler settings to use new compiler
    
    * Change Raise to Summon test to expect 0 errors instead
    
    * Update test references to match contract source modifications and
    compiler version

--- src/components/solc/index.js
+++ src/components/solc/index.js
@@ -26,7 +26,7 @@ export default class Solc {
         this.ref = null;
         this._counter = 0;
         this._cbMap = {};
-        this._version = "0.4.25";
+        this._version = "0.5.10";
     }
 
     init() {
@@ -45,7 +45,7 @@ export default class Solc {
 
         ReactDOM.render((
             <div style={{display: 'none'}} id={this.id}>
-                <iframe ref={setRef} src="/solc/index-v0.4.25b.html" frameBorder="0"></iframe>
+                <iframe ref={setRef} src="/solc/index-v0.5.10.html" frameBorder="0"></iframe>
             </div>
         ), document.getElementById('solc'));
 
