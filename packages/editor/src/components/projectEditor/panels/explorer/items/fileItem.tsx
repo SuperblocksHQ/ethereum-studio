2b9f0fe376d6889326ac41e4fd8833548b0b2c98
Spejbl <dedecekk@gmail.com>
2019-10-21 10:28:45 +0700

    Remove titles from tooltips

--- packages/editor/src/components/projectEditor/panels/explorer/items/fileItem.tsx
+++ packages/editor/src/components/projectEditor/panels/explorer/items/fileItem.tsx
@@ -18,12 +18,12 @@ export function getToolbar(props: IProps) {
     return (
         <div className={style.buttonsWrapper}>
             <div className={style.buttons} onClick={e => e.stopPropagation()}>
-                <a href='#' title='Rename file' onClick={() => props.onRenameClick(props.data.id)}>
+                <a href='#' onClick={() => props.onRenameClick(props.data.id)}>
                     <Tooltip delay='500' title='Rename'>
                         <IconEdit />
                     </Tooltip>
                 </a>
-                <a href='#' title='Delete file' onClick={() => props.onDeleteClick(props.data.id)} >
+                <a href='#' onClick={() => props.onDeleteClick(props.data.id)} >
                     <Tooltip delay='500' title='Delete'>
                         <IconTrash />
                     </Tooltip>
