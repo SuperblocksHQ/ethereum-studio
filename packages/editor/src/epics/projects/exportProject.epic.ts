9e817500df963a6d74d70c0e8657d67666a09eb2
Spejbl <dedecekk@gmail.com>
2019-10-30 12:17:20 +0700

    Update prompt while exporting file

--- packages/editor/src/epics/projects/exportProject.epic.ts
+++ packages/editor/src/epics/projects/exportProject.epic.ts
@@ -22,6 +22,7 @@ import { ProjectItemTypes } from '../../models';
 import { traverseTree } from '../../reducers/explorerLib';
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
+import { empty } from 'rxjs';
 
 function pathToString(path: string[]) {
     return '/' + path.join('/');
@@ -35,10 +36,19 @@ export const exportProject = (action$: AnyAction, state$: any) => action$.pipe(
         const zip = new JSZip();
         const fileName = state.projects.project.name.trim().toLowerCase().replace(/ /g, '_') + '_' + Date.now();
 
-        let includeBuildFiles = false;
-        if (confirm('Do you also want to save the project state (current contract addresses, ABIs, etc...)?')) {
-            includeBuildFiles = true;
+        const promptResult = prompt(
+            'Do you also want to save the project state (current contract addresses, ABIs, etc...)?',
+            'yes'
+        );
+        if (!promptResult) {
+            return empty();
         }
+        const promptResultLowerCase = promptResult.toLowerCase();
+        if (promptResultLowerCase !== 'yes' && promptResultLowerCase !== 'no') {
+            alert('Download aborted. Yes or No answer expected.');
+            return empty();
+        }
+        const includeBuildFiles = promptResultLowerCase === 'yes';
 
         // Add all files to zip variable
         traverseTree(files, (file, path) => {
