8dc077e13451137397bbc61696b116f448fc5be0
Kryštof Viktora <krystof.viktora@seznam.cz>
2019-11-01 10:34:27 +0700

    Fix compile contract forking (#62)
    * Add toast while forking project
    
    * Update compileContract epic

--- packages/editor/src/reducers/toast.reducer.ts
+++ packages/editor/src/reducers/toast.reducer.ts
@@ -38,6 +38,7 @@ export default function toastsReducer(state = initialState, action: AnyAction, r
     switch (action.type) {
         case projectsActions.UPDATE_PROJECT_SETTINGS_FAIL:
         case projectsActions.FORK_PROJECT:
+        case projectsActions.CREATE_FORKED_PROJECT:
         case projectsActions.CREATE_PROJECT_SUCCESS:
         case projectsActions.DELETE_PROJECT_FAIL:
         case projectsActions.RENAME_PROJECT_FAIL:
