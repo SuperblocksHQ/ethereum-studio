4890e15e49d598c54ff9f5b3cb7d7e02af7ef5ad
Kryštof Viktora <krystof.viktora@seznam.cz>
2019-11-01 10:33:47 +0700

    ES-64 - Display preview of md files (#37)
    * Add code icon
    
    * Add markdown preview
    
    * Fix package json
    
    * Small icon update
    
    * Change icon size
    
    * Add code color scheme & fix overflow of pre

--- packages/editor/src/components/projectEditor/panes/editor/markdownPreview.tsx
+++ packages/editor/src/components/projectEditor/panes/editor/markdownPreview.tsx
@@ -1,27 +1,38 @@
 // Copyright 2019 Superblocks AB
-// 
+//
 // This file is part of Superblocks Lab.
-// 
+//
 // Superblocks Lab is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation version 3 of the License.
-// 
+//
 // Superblocks Lab is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
+import React from 'react';
+import { Converter } from 'showdown';
+import showdownHighlight from 'showdown-highlight';
+import style from './style-markdown-preview.less';
 
-export function getFileExtension(filename: string) {
-    return filename.toLowerCase().split('.').pop();
+interface IProps {
+    markdown?: string;
 }
 
-export function isSmartContract(filename: string) {
-    return getFileExtension(filename) === 'sol';
-}
+export const MarkdownPreview = (props: IProps) => {
+    const converter: Converter = new Converter({
+        extensions: [showdownHighlight]
+    });
 
-export function isMarkdown(filename: string) {
-    return getFileExtension(filename) === 'md';
-}
+    return (
+        <div className={style.overflowFix}>
+            <div
+                className={style.markdownPreview}
+                dangerouslySetInnerHTML={{__html: converter.makeHtml(props.markdown ? props.markdown : '')}}
+            />
+        </div>
+    );
+};
