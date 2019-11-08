369f255c0a9b38c9f485232762126da0c5df1986
Kryštof Viktora <krystof.viktora@seznam.cz>
2019-11-08 10:23:10 +0700

    Add Welcome text into Template select modal (#95)
    * Make project template more appealing
    
    * Change flow on plus icon

--- packages/editor/src/components/modals/projectTemplateModal/ProjectTemplateModal.tsx
+++ packages/editor/src/components/modals/projectTemplateModal/ProjectTemplateModal.tsx
@@ -27,22 +27,23 @@ interface IProps {
     isProjectLoading: boolean;
     createProjectFromTemplate: (template: ITemplate) => void;
     hideModal: () => void;
+    hideWelcome: boolean;
 }
 
 interface IState {
-    selectedTemplate: ITemplate;
+    selectedTemplate: ITemplate | null;
 }
 
 export default class ProjectTemplateModal extends Component<IProps, IState> {
     state: IState = {
-        selectedTemplate: Templates.templates[0] as ITemplate
+        selectedTemplate: this.props.hideWelcome ? Templates.templates[0] as ITemplate : null
     };
 
     onCloseClickHandle = () => {
         this.props.hideModal();
     }
 
-    onTemplateSelected = (selectedTemplate: ITemplate) => {
+    onTemplateSelected = (selectedTemplate: ITemplate | null) => {
         this.setState({
             selectedTemplate,
         });
@@ -52,12 +53,14 @@ export default class ProjectTemplateModal extends Component<IProps, IState> {
         const { createProjectFromTemplate } = this.props;
         const { selectedTemplate } = this.state;
 
-        createProjectFromTemplate(selectedTemplate);
+        if (selectedTemplate) {
+            createProjectFromTemplate(selectedTemplate);
+        }
     }
 
     render() {
         const { templates } = Templates;
-        const { isProjectLoading } = this.props;
+        const { isProjectLoading, hideWelcome } = this.props;
         const { selectedTemplate } = this.state;
 
         return (
@@ -74,8 +77,8 @@ export default class ProjectTemplateModal extends Component<IProps, IState> {
                                 <ul>
                                     {  templates.map((template: ITemplate) => (
                                             <li key={template.id}
-                                                className={selectedTemplate.id === template.id ? style.selected : null}>
-                                                <div onClick={() => this.onTemplateSelected(template)}>{template.name}</div>
+                                                className={selectedTemplate && selectedTemplate.id === template.id ? style.selected : null}>
+                                                <div onClick={() => this.onTemplateSelected((!hideWelcome && selectedTemplate && selectedTemplate.id === template.id) ? null : template)}>{template.name}</div>
                                             </li>
                                         ))
                                     }
@@ -83,7 +86,18 @@ export default class ProjectTemplateModal extends Component<IProps, IState> {
                             </div>
                         <div className={style.templateListContainer}>
                             <div className={style.templateListArea}>
-                                <div dangerouslySetInnerHTML={{__html: selectedTemplate.description}} />
+                                { selectedTemplate
+                                ?   <div dangerouslySetInnerHTML={{__html: selectedTemplate.description}} />
+                                :   <div>
+                                        <h2>Welcome</h2>
+                                        <p>
+                                            Ethereum Studio is a tool for developers who want to learn about building on Ethereum.
+                                            The templates on the left side will teach you how to write a smart contract, deploy it to Ethereum,
+                                            and interact with the contracts through a web-based application.
+                                        </p>
+                                        <p>For quick start, check out this <a href='https://adobe.ly/32bZ3sv' target='_blank' rel='noopener noreferrer'>video</a>.</p>
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
@@ -96,7 +110,7 @@ export default class ProjectTemplateModal extends Component<IProps, IState> {
                         />
                         <StyledButton
                             type={StyledButtonType.Primary}
-                            isDisabled={Object.entries(selectedTemplate).length === 0}
+                            isDisabled={selectedTemplate ? Object.entries(selectedTemplate).length === 0 : true}
                             text='Create Project'
                             onClick={this.onCreateProjectHandle}
                             loadingText='Creating...'
