8c20ea13d17054d315e37e8c9c087fd93e1e3edc
donfrigo <donfrigo@gmail.com>
2019-11-15 14:39:36 +0100

    using hasUnstoredChanges

--- packages/editor/src/epics/compiler/compileContract.epic.ts
+++ packages/editor/src/epics/compiler/compileContract.epic.ts
@@ -14,52 +14,19 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import { from, interval, concat, of } from 'rxjs';
-import { switchMap, first, withLatestFrom } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { switchMap, withLatestFrom } from 'rxjs/operators';
 import { ofType, Epic } from 'redux-observable';
-import { explorerActions, compilerActions, panelsActions, projectsActions } from '../../actions';
-import { compilerService } from '../../services';
-import { IPane, Panels } from '../../models/state';
-import { projectSelectors } from '../../selectors';
-import { panesSelectors } from '../../selectors/panes.selectors';
-
-function compileContract(compilerState: any) {
-    return from(
-        new Promise((resolve) => {
-            compilerService.queue(
-                { input: JSON.stringify(compilerState.input), files: compilerState.files },
-                (data: any) => resolve(compilerActions.handleCompileOutput(JSON.parse(data)))
-            );
-        })
-    );
-}
-
-function initCompilation(state: any) {
-    const project = projectSelectors.getProject(state);
-    const files = state.explorer.tree;
-    const isOwnProject = state.projects.isOwnProject;
-
-    if (isOwnProject) {
-        compilerService.init();
-        return concat(
-            of(panelsActions.openPanel(Panels.OutputLog)), // show output
-            interval(200).pipe(
-                first(() => compilerService.isReady()), // compiler has to be ready to be able to do smth
-                switchMap(() => concat(
-                    of(compilerActions.compilerReady(compilerService.getVersion())),
-                    compileContract(state.compiler)
-                ))
-            )
-        );
-    } else {
-        return [projectsActions.createForkedProject(project.name, project.description, files)];
-    }
-}
+import { explorerActions, compilerActions, projectsActions } from '../../actions';
+import { IPane } from '../../models/state';
+import { explorerSelectors, panesSelectors } from '../../selectors';
 
 export const compileContractsEpic: Epic = (action$: any, state$: any) => action$.pipe(
     ofType(explorerActions.COMPILE_CONTRACT),
     withLatestFrom(state$),
-    switchMap(([_action, state]) => {
+    switchMap(([action]) => {
+
+        const hasUnstoredChanges = explorerSelectors.hasUnstoredChanges(state$.value);
 
         const panes = panesSelectors.getPanes(state$.value);
         const hasUnsavedChanges = panes.reduce((acc: any, curr: IPane) => {
@@ -69,11 +36,10 @@ export const compileContractsEpic: Epic = (action$: any, state$: any) => action$
             return curr.hasUnsavedChanges;
         }, []);
 
-        if (hasUnsavedChanges) {
-            // Autosave
-            return concat(of(projectsActions.saveProject()), from(initCompilation(state$.value)));
+        if (hasUnsavedChanges || hasUnstoredChanges) {
+            return of(projectsActions.saveProject(action.data));
         }
 
-        return from(initCompilation(state));
+        return of(compilerActions.initCompilation(action.data));
     })
 );
