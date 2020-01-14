42897b68146ae7def82f91fc9c692ed15b95d302
Spejbl <dedecekk@gmail.com>
2020-01-14 12:39:41 +0530

    Make animated side buttons

--- packages/editor/src/components/projectEditor/sideButton.js
+++ packages/editor/src/components/projectEditor/sideButton.js
@@ -5,7 +5,7 @@ import style from './style.less';
 export function SideButton(props) {
     return(
         <button
-            className={classNames([style.sideButton, 'btnNoBg'])}
+            className={classNames([style.sideButton, 'btnNoBg', props.className])}
             onClick={props.onClick}
         >
             {props.icon}
