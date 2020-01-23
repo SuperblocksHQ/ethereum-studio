c784b445e1e462ae6aae39ec0ad67ec57d0e0b85
Javier Tarazaga <javier.tarazaga@valtech.se>
2020-01-23 21:36:12 +0100

    Added auto-render when saving app files

--- packages/editor/src/reducers/preview.reducer.ts
+++ packages/editor/src/reducers/preview.reducer.ts
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Superblocks Lab.  If not, see <http://www.gnu.org/licenses/>.
 
-import { previewActions, explorerActions, deployerActions, projectsActions } from '../actions';
+import { previewActions, explorerActions, deployerActions, projectsActions, panesActions } from '../actions';
 import { AnyAction } from 'redux';
-import { findItemByPath } from './explorerLib';
+import { findItemByPath, findItemById } from './explorerLib';
 import { ProjectItemTypes } from '../models';
 import { projectSelectors } from '../selectors';
 
@@ -52,41 +52,55 @@ function errorHtml(message: string) {
     </html>`;
 }
 
+function generateHtmlToRender(rootState: any) {
+    const tree = rootState.explorer.tree;
+    const addresses = [projectSelectors.getSelectedAccount(rootState).address];
+
+    const html = findItemByPath(tree, [ 'app', 'app.html' ], ProjectItemTypes.File);
+    const css = findItemByPath(tree, [ 'app', 'app.css' ], ProjectItemTypes.File);
+    const js = findItemByPath(tree, [ 'app', 'app.js' ], ProjectItemTypes.File);
+
+    let contractJs = '';
+    const contractListFolder = findItemByPath(tree, [ 'build', 'contracts' ], ProjectItemTypes.Folder);
+    if (contractListFolder && contractListFolder.children.length > 0) {
+        for (const contractFolder of contractListFolder.children) {
+            const contractFiles = contractFolder.children.filter((file) => file.name.includes(`${rootState.projects.selectedEnvironment.name}.js`));
+            for (const file of contractFiles) {
+                contractJs += file.code + '\n';
+            }
+        }
+    }
+
+    if ((!html || !css || !js || !html.code || !css.code || !js.code)) {
+        return errorHtml('There was an error rendering your project');
+    } else {
+        return getInnerContent(html.code, css.code, contractJs + '\n' + js.code, rootState.projects.selectedEnvironment.endpoint, addresses);
+    }
+}
+
 export default function previewReducer(state = initialState, action: AnyAction, rootState: any) {
     switch (action.type) {
         case deployerActions.DEPLOY_SUCCESS:
         case explorerActions.INIT_EXPLORER_SUCCESS:
         case projectsActions.SET_ENVIRONMENT_SUCCESS:
         case previewActions.REFRESH_CONTENT:
-            let htmlToRender;
-            const tree = rootState.explorer.tree;
-            const addresses = [projectSelectors.getSelectedAccount(rootState).address];
-
-            const html = findItemByPath(tree, [ 'app', 'app.html' ], ProjectItemTypes.File);
-            const css = findItemByPath(tree, [ 'app', 'app.css' ], ProjectItemTypes.File);
-            const js = findItemByPath(tree, [ 'app', 'app.js' ], ProjectItemTypes.File);
-
-            let contractJs = '';
-            const contractListFolder = findItemByPath(tree, [ 'build', 'contracts' ], ProjectItemTypes.Folder);
-            if (contractListFolder && contractListFolder.children.length > 0) {
-                for (const contractFolder of contractListFolder.children) {
-                    const contractFiles = contractFolder.children.filter((file) => file.name.includes(`${rootState.projects.selectedEnvironment.name}.js`));
-                    for (const file of contractFiles) {
-                        contractJs += file.code + '\n';
-                    }
-                }
-            }
-
-            if ((!html || !css || !js || !html.code || !css.code || !js.code)) {
-                htmlToRender = errorHtml('There was an error rendering your project');
-            } else {
-                htmlToRender = getInnerContent(html.code, css.code, contractJs + '\n' + js.code, rootState.projects.selectedEnvironment.endpoint, addresses);
-            }
-
             return {
                 ...state,
-                htmlToRender,
+                htmlToRender: generateHtmlToRender(rootState),
             };
+        case panesActions.SAVE_FILE_SUCCESS:
+            const result = findItemById(rootState.explorer.tree, action.data.fileId);
+            // reload dapp file data if the file was updated
+            if (result.item && (result.item.name.endsWith('.css') ||
+                result.item.name.endsWith('.html') ||
+                result.item.name.endsWith('.js'))) {
+                return {
+                    ...state,
+                    htmlToRender: generateHtmlToRender(rootState),
+                };
+            } else {
+                return state;
+            }
         default:
             return state;
     }
