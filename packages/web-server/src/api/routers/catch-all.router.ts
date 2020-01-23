3f8ce4bf06b4b33a147f9b0795c11116628347e7
Spejbl <dedecekk@gmail.com>
2020-01-23 13:38:43 +0530

    Remove writing to the file

--- packages/web-server/src/api/routers/catch-all.router.ts
+++ packages/web-server/src/api/routers/catch-all.router.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from 'express';
-import path from 'path';
 import { InjectEnvVariablesCommand } from '../../app';
-import { publicDirectory } from '../../common';
 
 // Default "catch-all" route serving the Ethereum Studio editor
 export const catchAllRouter = express.Router();
 
 catchAllRouter.get('/*', async (_req: Request, res: Response) => {
     const command = new InjectEnvVariablesCommand();
-    await command.execute();
-    res.sendFile(path.join(publicDirectory, 'index.html'));
+    await command.execute(res);
 });
--- packages/web-server/src/app/commands/inject-env-variables.command.ts
+++ packages/web-server/src/app/commands/inject-env-variables.command.ts
@@ -1,26 +1,24 @@
+import { Response } from 'express';
 import path from 'path';
 import fs from 'fs';
 import { publicDirectory } from '../../common';
 
 export class InjectEnvVariablesCommand {
 
-    async execute() {
+    async execute(res: Response) {
         const filePath = path.join(publicDirectory, 'index.html');
 
         fs.readFile(filePath, 'utf8', (readErr, file) => {
-            if (readErr) { throw readErr; }
-
+            if (readErr) {
+                throw readErr;
+            }
             const envVariables = {
                 AMPLITUDE_API_KEY: process.env.AMPLITUDE_API_KEY,
                 API_BASE_URL: process.env.API_BASE_URL
             };
 
-            // Inject environment variables into index.html
-            file = file.replace(/__ENV_VARIABLES__/g, JSON.stringify(envVariables));
-
-            fs.writeFile(filePath, file, 'utf-8', (writeErr) => {
-                if (writeErr) { throw writeErr; }
-            });
+            // Inject environment variables into index.html and send response
+            res.send(file.replace(/__ENV_VARIABLES__/g, JSON.stringify(envVariables)));
         });
     }
 }
