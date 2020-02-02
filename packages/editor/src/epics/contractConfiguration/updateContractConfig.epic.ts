be4dd0aade9691d69feaedd18763262ecc8460c0
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2020-02-02 21:06:02 +0100

    auto update for addding extra arguments to the constructor, deleting in progress

--- packages/editor/src/epics/contractConfiguration/updateContractConfig.epic.ts
+++ packages/editor/src/epics/contractConfiguration/updateContractConfig.epic.ts
@@ -5,8 +5,10 @@ import { withLatestFrom, switchMap, catchError } from 'rxjs/operators';
 import { IContractConfiguration, IProjectItem, ProjectItemTypes } from '../../models';
 import { findItemByPath, traverseTree } from '../../reducers/explorerLib';
 import { empty, of } from 'rxjs';
+import { ConstructorArgumentsList } from '../../components/projectEditor/editors/contractConfigModal/constructorArgumentsList';
+import { number } from 'prop-types';
 
-export const updateContractConfig = (action$: AnyAction, state$: any, rootState: any) => action$.pipe(
+export const updateContractConfig = (action$: AnyAction, state$: any) => action$.pipe(
     ofType(compilerActions.HANDLE_COMPILE_OUTPUT),
     withLatestFrom(state$),
     switchMap(([action, state]) => {
@@ -16,9 +18,40 @@ export const updateContractConfig = (action$: AnyAction, state$: any, rootState:
         const compilerOutputData = action.data.contracts[firstKey][secondKey].metadata;
         const parsedCompilerOutputData = JSON.parse(compilerOutputData);
         const constructorData = parsedCompilerOutputData.output.abi.filter((obj: { type: string; }) => obj.type === 'constructor');
+        console.log('constructor data', constructorData);
         const numberOfArgs = constructorData[0].inputs.length;
-        const rootItem = state$.value.explorer.tree.children;
-        return contractConfigActions.CLOSE_CONTRACT_CONFIG;
+        const dappFileItem: Nullable<IProjectItem> = findItemByPath(state.explorer.tree, ['dappfile.json'], ProjectItemTypes.File);
+        console.log('BRUH', dappFileItem);
+        if (dappFileItem != null && dappFileItem.code != null) {
+            const code = dappFileItem.code;
+            const parsedCode = JSON.parse(code);
+            console.log('parsed code', parsedCode);
+            console.log('NUMBER OF ARGS', numberOfArgs);
+            console.log('DAPP FILE ARGS', parsedCode.contracts[0].args.length);
+            if (numberOfArgs > parsedCode.contracts[0].args.length) {
+                for (const el of constructorData[0].inputs.slice(1)) {
+
+                    parsedCode.contracts[0].args.push({ type: 'value', value: el.name });
+                }
+                console.log('after all', parsedCode);
+                return [panesActions.saveFile(dappFileItem.id, JSON.stringify(parsedCode, null, 4))];
+            }
+            //TODO: not working as intended, find a better way
+            if (numberOfArgs < parsedCode.contracts[0].args.length) {
+                for (let el of constructorData[0].inputs) {
+                    el = { type: 'value', value: el.name };
+                    const arr = [el];
+                    parsedCode.contracts[0].args = arr;
+                }
+                console.log('after all 2', parsedCode);
+                return [panesActions.saveFile(dappFileItem.id, JSON.stringify(parsedCode, null, 4))];
+            } else {
+                return empty();
+            }
+        } else {
+            return empty();
+        }
+
     }),
     catchError((err: any) => {
         console.log('Error while updating the contract configuration: ', err);
