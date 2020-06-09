3a7eeda18e224587ecba1663e1ecfea306dd733e
Sam Richards <sbrichards@gmail.com>
2020-06-08 18:36:37 -0700

    Update Token.sol code comments

--- packages/editor/templates/Token/contracts/Token.sol
+++ packages/editor/templates/Token/contracts/Token.sol
@@ -1,44 +1,37 @@
 pragma solidity ^0.5.10;
 
-// TODO remove duplicate explanations from HelloWorld.sol
 contract Token {
-    // An Ethereum address is used to identify an account, similar to an email address.
+    // An Ethereum address is similar to an email address. It's used to identify an account.
     // Addresses can can represent a smart contract or an external (user) accounts.
-    // Learn more: https://solidity.readthedocs.io/en/v0.5.3/types.html#address
+    // Learn more: https://solidity.readthedocs.io/en/v0.5.10/types.html#address
     address public owner;
 
     // A mapping is essentially a hash table data structure.
     // This mapping assigns an unsigned integer (the token balance) to an address (the token holder).
-    // Learn more: https://solidity.readthedocs.io/en/v0.5.3/types.html#mapping-types
+    // Learn more: https://solidity.readthedocs.io/en/v0.5.10/types.html#mapping-types
     mapping (address => uint) public balances;
-
 
     // Events allow for logging of activity on the blockchain.
     // Ethereum clients can listen for events in order to react to contract state changes.
-    // Learn more: https://solidity.readthedocs.io/en/v0.4.21/contracts.html#events
+    // Learn more: https://solidity.readthedocs.io/en/v0.5.10/contracts.html#events
     event Transfer(address from, address to, uint amount);
 
-    // Constructors are special function that is only executed during contract creation.
-    // Initialize the contract's data, setting the `owner`
-    // Learn more: https://solidity.readthedocs.io/en/latest/contracts.html#constructors
+    // Initializes the contract's data, setting the `owner`
+    // to the address of the contract creator.
     constructor() public {
-        // Uses the special msg global variable to store the
-        // address of the contract creator
-        owner = msg.sender;
-
-        // What is the `msg`?
-        // All smart contracts rely on a external transaction to trigger it's functions
-        // `msg` is a global variable that includes relevant data of the given transaction,
-        // Such as the address that sent the transaction and the ETH value included in the transation
+        // All smart contracts rely on external transactions to trigger its functions.
+        // `msg` is a global variable that includes relevant data on the given transaction,
+        // such as the address that sent the transaction and the ETH value included in the transation.
         // Learn more: https://solidity.readthedocs.io/en/latest/units-and-global-variables.html#block-and-transaction-properties
+        owner = msg.sender;
     }
 
-    // Sends an amount of newly created tokens to an address
+    // Creates an amount of new tokens and sends them to an address.
     function mint(address receiver, uint amount) public {
         // `require` is a control structure used to enforce certain conditions.
         // If a `require` statement evaluates to `false`, an exception is triggered,
-        // which reverts all changes made to the state during the current call
-        // Learn more: https://solidity.readthedocs.io/en/v0.4.24/control-structures.html#error-handling-assert-require-revert-and-exceptions
+        // which reverts all changes made to the state during the current call.
+        // Learn more: https://solidity.readthedocs.io/en/v0.5.10/control-structures.html#error-handling-assert-require-revert-and-exceptions
 
         // Only the contract owner can call this function
         require(msg.sender == owner, "You are not the owner.");
@@ -50,13 +43,12 @@ contract Token {
         balances[receiver] += amount;
     }
 
-    // Sends an amount of existing tokens
-    // from any caller to an address
+    // Sends an amount of existing tokens from any caller to an address.
     function transfer(address receiver, uint amount) public {
         // The sender must have enough tokens to send
         require(amount <= balances[msg.sender], "Insufficient balance.");
 
-        // Adjusts address balances
+        // Adjusts token balances of the two addresses
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
 
